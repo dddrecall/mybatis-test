ea6aa4e340df1156afdc22ddbd8d9428cf667d27
dddrecall <dddrecall@foxmail.com>
2015-12-20 22:34:19 +0800

    111
    111

--- src/com/recall/mybatis/test/TestOne2Many2.java
+++ src/com/recall/mybatis/test/TestOne2Many2.java
@@ -2,7 +2,7 @@ package com.recall.mybatis.test;
 
 import java.io.IOException;
 import java.io.InputStream;
-
+    
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
